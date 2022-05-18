27bb7f2772f579ef31f29a6b71806ddba8786325
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-18 17:00:18 +0200

    inserimento delle card nel container

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //creo l'array di oggetti con tutti i membri 
 const card = [
-    {
-        name : "wayne barnett",
-        role : "Founder & ceo",
-        image : "wayne-barnett-founder-ceo.jpg"
-    },
+    // {
+    //     name : "wayne barnett",
+    //     role : "Founder & ceo",
+    //     image : "wayne-barnett-founder-ceo.jpg"
+    // },
 
     {
         name : "Angela Caroll",
@@ -43,3 +43,52 @@ const card = [
 ];
 
 console.log(card);
+
+//variabile con l'inserimento della classe container
+const cont = document.querySelector(".team-container");
+
+ let insertCardbool = false; 
+
+//esegui la funzione
+insertCard();
+//costruiamo una funzione che crea ttutte le card e le aggiunge al container
+function insertCard() {
+    for (let i = 0; i < card.length; i++) {
+        let newCard = createCard(card[i]);
+        //aggiungiamo la card al container
+        cont.append(newCard);
+        
+    }
+    
+}
+
+
+
+//costruiamo una funzione che crea tutti gli elementI html e li lega al dom
+function createCard(member) {
+    //crea una elemento 
+    let newCard = document.createElement('div');
+    newCard.classList.add("team-card")
+
+    //crea un contenitore immagine
+    let boxImg = document.createElement('div');
+    boxImg.classList.add("card-image");
+
+    //crea l'immagine contenuta
+    let newImg = document.createElement('img');
+    //controllo se l'immagine e nella cartella img
+    if (!insertCardbool) {
+        newImg.src = "img/" + member.image;
+    } else {
+        newImg.src = member.image;
+    }
+      //alt per l'immagine
+      newImg.alt = member.name
+
+    //aggiungi gli elementi uno dentro l'altro
+    boxImg.append(newImg);
+    newCard.append(boxImg);
+
+   return newCard;
+    
+}
