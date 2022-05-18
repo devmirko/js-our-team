66dc6f110115a0a53050c5a65e429c73d334200e
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-18 16:01:48 +0200

    creazione array con oggetti

---
+++ script.js
@@ -0,0 +1,45 @@
+//creo l'array di oggetti con tutti i membri 
+const card = [
+    {
+        name : "wayne barnett",
+        role : "Founder & ceo",
+        image : "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        name : "Angela Caroll",
+        role : "Chief Editor",
+        image : "angela-caroll-chief-editor.jpg"
+
+    },
+
+    {
+        name : "Walter Gordon",
+        role : "Office Manager",
+        image : "walter-gordon-office-manager.jpg"
+
+    },
+
+    {
+        name : "Angela Lopez",
+        role : "Social Media Menager",
+        image : "angela-lopez-social-media-manager.jpg"
+
+    },
+
+    {
+        name : "Scott Estrada",
+        role : "Developer",
+        image : "scott-estrada-developer.jpg"
+
+    },
+
+    {
+        name : "Barbara Ramos",
+        role : "Graphic Designer",
+        image : "barbara-ramos-graphic-designer.jpg"
+
+    },
+];
+
+console.log(card);
